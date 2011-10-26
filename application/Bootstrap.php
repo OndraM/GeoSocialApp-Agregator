f48f1b80c2f710ba41740b1d2fa86f3d6819c7f1
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-10-26 04:18:07 +0200

    Init environment

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,14 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     
+    protected function _initEnvironment() {
+        mb_internal_encoding('utf-8');
+        date_default_timezone_set('Europe/Prague');
+
+        ini_set('session.gc_maxlifetime', 60*60*24*60);     // 60 days
+        ini_set('session.cookie_lifetime', 60*60*24*180);   // 180 days
+    }
+    
     protected function _initDoctype() {
         $this->bootstrap('view');
         $view = $this->getResource('view');
