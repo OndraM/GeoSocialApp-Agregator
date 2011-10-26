e800ed76c219b651375c4368c176e5b14c2be87b
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-10-26 04:21:45 +0200

    Configure own view helpers and libraries structure, add Script view helper

---
+++ application/views/helpers/Script.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Helper for adding javascript snippet to view
+ *
+ * @author  Ondřej Machulda <ondrej.machulda@gmail.com>
+ *
+ */
+
+
+class GSAA_View_Helper_Script extends Zend_View_Helper_Abstract
+{
+
+    /**
+     * Add and pack Javascript code to view script, to be executed on document DOM load.
+     * Require jQuery being loaded first.
+     * 
+     * In case $inPlace is false, script is apended to header (default). 
+     * Otherwise its returned as string.
+     *
+     * @param type $script
+     * @param type $inPlace
+     * @param type $pack
+     * @return string 
+     */
+    public function script($script, $inPlace = false, $pack = true)
+    {
+        // $script empty
+        if (empty($script)) {
+            return;
+        }
+
+        // remove some nonprinatble characters 1c-1f (cause unexpected string literal error)
+        $script = preg_replace('/[\x1c-\x1f]/', '', $script);
+
+        $return = "";
+        $output = "$(document).ready(function(){\n"
+                . $script
+                . "});";
+
+        if ($pack) { // pack script
+            $filter = new GSAA_Filter_JavascriptPacker();
+            $output = $filter->filter($output);
+        }
+        
+        if (!$inPlace) { // append script into head
+            $this->view->headScript()->appendScript($output . "\n");
+            return;
+        }
+        // return script 
+        $return .= "<script type=\"text/javascript\">\n"
+                . "\t//<![CDATA[\n";
+        $return .= $output;
+        $return .= "\n"
+                . "\t//]]>\n"
+                . "</script>\n";
+        
+        return $return;
+
+    }
+
+}
---
+++ library/GSAA/Filter/JavascriptPacker.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Pack a given Javascript code
+ * 
+ * @author  Ondřej Machulda <ondrej.machulda@gmail.com>
+ *
+ */
+
+class GSAA_Filter_JavascriptPacker implements Zend_Filter_Interface
+{
+    /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+        Zend_Loader::loadFile("jsmin.php", null, true);
+    }
+    
+    /**
+     * Pack (minify) content of $value
+     * 
+     * @param type $value
+     * @return type 
+     */
+    public function filter($value)
+    {
+        return JSMin::minify($value);
+    }
+}
