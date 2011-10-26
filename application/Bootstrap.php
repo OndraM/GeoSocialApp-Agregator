189f6581bbce729526fb86bddf29c5cdbd902127
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-10-26 02:34:22 +0200

    Layout basics

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    
+    protected function _initDoctype() {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('HTML5');
+    }
 
 
 }
