2a3aac1ce867b34f6578af63f00434b6ee7c0437
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-10-27 21:47:31 +0200

    POI controller, inc. own ajax context action helper

---
+++ application/controllers/PoiController.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Controller for requesting POIs and POIs sets using AJAX
+ */
+
+class PoiController extends Zend_Controller_Action
+{
+
+    protected $_foursquareModel = null;
+
+    public function init()
+    {
+        $this->_foursquareModel = new GSAA_Model_LBS_Foursquare();
+        
+        $ajaxContext = $this->_helper->getHelper('AjaxContext');
+        
+        $ajaxContext->addActionContext('get-nearby', 'json')
+                    ->initContext();
+    }
+
+    public function getNearbyAction()
+    {
+        // TODO: load params (getParam);
+        $lat = 50.076738;
+        $long = 14.41803;
+        
+        //echo "<pre>";
+        //echo Zend_Json::prettyPrint($this->_foursquareModel->getNearbyVenues($lat, $long));
+        //echo Zend_Json::encode($this->_foursquareModel->getNearbyVenues($lat, $long));
+        //echo "</pre>";
+        $venues = $this->_foursquareModel->getNearbyVenues($lat, $long);
+        $this->view->venues = $venues['venues'];
+        
+        // overwrite context setting for testing purposes // TODO
+        $response = $this->getResponse();
+        $response->setHeader('Content-Type', 'text/html');
+    }
+
+
+}
+
+
+
---
+++ application/controllers/helpers/AjaxContext.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Removes need to specify format parameter in url when providing only one ajax context.
+*/
+class GSAA_Controller_Action_Helper_AjaxContext
+    extends Zend_Controller_Action_Helper_AjaxContext
+{
+    /**
+     * Automatically initialise the first and only context
+     */
+    public function initContext($format = null) {
+        $request = $this->getRequest();
+        $action = $request->getActionName();
+        $context = $this->getActionContexts($action);
+
+        //check in case multiple contexts DO exist
+        if(count($context) === 1) { 
+            $format = $context[0]; 
+
+        }
+        return parent::initContext($format);
+    }
+}
