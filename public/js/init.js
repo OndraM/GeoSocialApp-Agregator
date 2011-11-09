da7274cc665831a584a667c4a69b64d5ae0b6e3f
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-11-09 03:57:29 +0100

    Google Maps API basics; listener to map.dragend

--- public/js/init.js
+++ public/js/init.js
@@ -22,3 +22,46 @@ function doGeolocate() {
         );
     }
 }
+
+var map;
+function mapInit() {
+    var latlng;
+    if ( $('#searchform input[name=lat]').val() != ''
+        && $('#searchform input[name=long]').val() != '' ) { // if set, get location from form values
+        latlng = new google.maps.LatLng($('#searchform input[name=lat]').val(),
+                                            $('#searchform input[name=long]').val());
+    } else { // otherwise use default location
+        latlng = new google.maps.LatLng(50.087811, 14.42046);
+    }
+    
+    var mapOptions = {
+        zoom: 14,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(
+        document.getElementById('venues-map'),
+        mapOptions
+    );
+        
+    google.maps.event.addListener(map, 'dragend', function() {
+		var latlng = map.getCenter();
+        //console.log(latlng.toUrlValue());
+        $('#searchform input[name=lat]').val(latlng.lat().toFixed(6));
+        $('#searchform input[name=long]').val(latlng.lng().toFixed(6));
+        $('#searchform').submit();
+	});
+}
+
+function setMapCenter(lat, lng) {
+    if (map != undefined) {
+        var newMapCenter = new google.maps.LatLng(lat, lng);
+        var mapCenter = map.getCenter();
+        if (!mapCenter.equals(newMapCenter)) {
+            map.panTo(newMapCenter);
+        }
+    }
+    
+    
+}
+
