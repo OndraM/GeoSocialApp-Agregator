637471bb2f03d4c7db3be0220dd30541b6dcf0d3
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-11-22 19:11:14 +0100

    Function to merge similar GSAA_Model_POI to GSAA_Model_AggregatedPOI (first part of #30)

--- application/controllers/PoiController.php
+++ application/controllers/PoiController.php
@@ -77,29 +77,60 @@ class PoiController extends Zend_Controller_Action
             }
         }      
         
+        $pois = $this->_mergePois($pois_raw);
+                
+        print_r($pois);
+    }
+    
+    
+    /**
+     * Merge array of GSAA_Model_POI
+     * 
+     * @param array $pois_raw
+     * @return array Array of GSAA_Model_AggregatedPOI
+     */    
+    protected function _mergePois(array $pois_raw) {                
+        $pois = array(); // Array of GSAA_Model_AggregatedPOI
         for ($x = 0; $x < count($pois_raw); $x++) {
-            //d($pois_raw[$x], $x);
+            if (is_null($pois_raw[$x])) continue; // skip already merged items
+            $agPoi = new GSAA_Model_AggregatedPOI();
+            $agPoi->addPoi($pois_raw[$x]); // copy entire POI
+            
+            
             echo $x . ": " . $pois_raw[$x]->name . "<br />\n";
             for ($y = 0; $y < count($pois_raw); $y++) {
+                if (is_null($pois_raw[$y])) continue; // skip already merged items
                 if ($x == $y) continue; // skip the same POI
-                $similair_percent = 0;
-                similar_text($pois_raw[$x]->name, $pois_raw[$y]->name, $similair_percent);
-                if ($similair_percent > 75) {
-                    echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $y . ": " . $pois_raw[$y]->name . " | "
-                        . 'similair_text: ' . round($similair_percent, 1) . " | "
-                        . 'distance: '
-                        . $this->_serviceModels[$pois_raw[$x]->type]->getDistance(
+                
+                $similar_percent = 0;
+                /* TODO: convert name to ASCII, to get better results
+                 * Test case: 50.076738, 14.5; Strasnicke divadlo
+                 */
+                /*
+                 * TODO: other text matching improvenets (maybe remove some chars etc.)
+                 */
+                similar_text($pois_raw[$x]->name, $pois_raw[$y]->name, $similar_percent);
+                
+                $distance = $this->_serviceModels[$pois_raw[$x]->type]->getDistance(
                                 $pois_raw[$x]->lat,
                                 $pois_raw[$x]->lng,
                                 $pois_raw[$y]->lat,
-                                $pois_raw[$y]->lng)
-                        . "<br />\n";
-                }
+                                $pois_raw[$y]->lng);
+                
+                /*echo "&nbsp;&nbsp;&nbsp;&nbsp;" . $y . ": " . $pois_raw[$y]->name . " | "
+                        . 'similar_text: ' . round($similar_percent, 1) . " | "
+                        . 'distance: '
+                        . $distance
+                        . "<br />\n";*/
+                if ($similar_percent > 75 && $distance < 150) { // Merge objects
+                    
+                    $agPoi->addPoi($pois_raw[$y]); // copy entire POI
+                    $pois_raw[$y] = null; // remove content from array, so that the POI wont be merged again
+                } 
             }
+            $pois[] = $agPoi;
         }
-        
-        
-        //d($pois);
+        return $pois;
     }
 
 
