8c92553f08f14ea9e07e3b4eca993699317ddb74
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-11-25 17:28:01 +0100

    Link in infoWindow to fire Fancybox pop-up with ajax content

--- public/js/init.js
+++ public/js/init.js
@@ -12,16 +12,19 @@ $(document).ready(function() {
     
     // Fancubox popUp windows
     $(".popUp").fancybox({
-		maxWidth	: 800,
-		/*maxHeight	: 600,*/
-		fitToView	: false,
-		width		: '70%',
-		height		: '70%',
-		autoSize	: false,
-		closeClick	: false,
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});
+        maxWidth	: 800,
+        /*maxHeight	: 600,*/
+        fitToView	: false,
+        width		: '70%',
+        height		: '70%',
+        autoSize	: false,
+        closeClick	: false,
+        openEffect	: 'none',
+        closeEffect	: 'none',
+        helpers:  {
+            title : null
+        }
+    });
     
 });
 
@@ -298,6 +301,11 @@ function addPoisOnMap(pois) {
             });
             content += '</ul>';
         }
+        
+        detailUrl = '/poi/show-detail'; // TODO paramaters
+        content += '<div>' 
+                + '<a href="' + detailUrl + '" class ="popUp fancybox.ajax" title="Show all details of this venue">Show details &raquo;</a>'
+                + '</div>';        
         
         content += '</div>';
 
