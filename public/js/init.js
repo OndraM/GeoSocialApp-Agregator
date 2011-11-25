17963932c052017f023484c7b5fa0aadf53270d7
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-11-25 16:59:24 +0100

    Fancybox included

--- public/js/init.js
+++ public/js/init.js
@@ -6,6 +6,23 @@ $(document).ready(function() {
 	$('a[href^="http://"]').click(function() {
 		this.target = '_blank';
 	});
+    
+    // Fancybox init
+    $(".fancybox").fancybox();
+    
+    // Fancubox popUp windows
+    $(".popUp").fancybox({
+		maxWidth	: 800,
+		/*maxHeight	: 600,*/
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+    
 });
 
 /*
