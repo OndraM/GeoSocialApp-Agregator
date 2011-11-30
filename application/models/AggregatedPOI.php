79968a77bc0a53f95ac48524562b4830855ab704
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-11-30 21:46:54 +0100

    Update phpDoc

--- application/models/AggregatedPOI.php
+++ application/models/AggregatedPOI.php
@@ -3,18 +3,21 @@
 class GSAA_Model_AggregatedPOI
 {
     
-    /*
+    /**
      * Array of GSAA_Model_POI
      */
     protected $_pois    = array();
     
-    /*
-     * Flag to indicate whether POIS were already sorted
+    /**
+     * Flag to indicate whether POIS are in sorted state
      */
     protected $_sorted = false;
         
-    /* 
+    /** 
      * Get array of aggregated POI types {fq, gw, gg, fb}
+     * Note each can eventually occur more than once.
+     * 
+     * @return Array Array of aggreagted POI types. 
      */    
     public function getTypes() {
         $this->_sortPois();
@@ -25,43 +28,41 @@ class GSAA_Model_AggregatedPOI
         return $return;
     }
     
-    /*
+    /**
      * Get aggregated id of POI
      */
     public function getId() {
         $this->_sortPois();
-        // return ID of primary POI
-        return reset($this->getPois())->id;
+        return reset($this->getPois())->id; // return ID of the primary POI
     }
 
-    /* 
+    /**
      * Get name of aggregated POI
      */
     public function getName() {
         $this->_sortPois();
-        // return name of primary POI
-        return reset($this->getPois())->name;
+        return reset($this->getPois())->name; // return name of the primary POI
     }
     
-    /*
+    /**
      * Get latitude of aggregated POI
      */
     public function getLat() {
         $this->_sortPois();
+        return reset($this->getPois())->lat; // return latitude of the primary POI
         // TODO: mayby get average location?
-        return reset($this->getPois())->lat;
     }
 
-    /*
+    /**
      *  Get longitude of aggregated POI
      */
     public function getLng() {
         $this->_sortPois();
+        return reset($this->getPois())->lng; // return longitude of the primary POI
         // TODO: mayby get average location?
-        return reset($this->getPois())->lng;
     }
 
-    /*
+    /**
      *  Get distance of aggregated lat & lng in meters from search coords (if available)
      */
     public function getDistance() {
@@ -69,48 +70,59 @@ class GSAA_Model_AggregatedPOI
         return reset($this->getPois())->distance;
     }
 
-    /*
-     * Get aggregated address (if available), or find address on POI location
+    /**
+     * Get aggregated address (if available), or find address on POI location (TODO!)
+     
+     * @param bool If none adres found, should we do reverse geocoding and try to found it
+     * @return type 
      */
-    public function getAddress() {
+    public function getAddress($find = false) {
         $this->_sortPois();
-        // TODO - get best address, if none of primary POI, try onother ones, 
-        // if none of them has address, maybe search on Google :)?
+        if ($find) {
+            // TODO - get best address, if none of primary POI, try onother ones, 
+            // if none of them has address, maybe search on Google :)?
+        }
         return reset($this->getPois())->address;
     }
     
-    /*
-     * Get URL of aggregated detail (maybe unnecesarry)
+    /**
+     * Get URL of aggregated detail
      */
     public function getUrl() {
         $this->_sortPois();
         // TODO - combine url
         return;
-    }
+        }
 
-    /*
+    /**
      * Get current array of GSAA_Model_POI
+     * 
+     * @return array Array of GSAA_Model_POI
      */
     public function getPois() {
         $this->_sortPois();
         return $this->_pois;
     }
     
-    /*
+    /**
      * Add POI into aggregated one.
+     * 
+     * @param GSAA_Model_POI POI to add
      */
     public function addPoi(GSAA_Model_POI $poi) {
         $this->_pois[] = $poi;
-        $this->_sorted = false; // indicate POIs are not sorted
+        $this->_sorted = false; // indicate POIs are not in sorted state
     }
     
-    /*
-     * Sort POIs in $this->_pois array
+    /**
+     * Sort POIs in $this->_pois array.
+     * The purpose why this is not called automatically after each addPoi() is performance.
+     * We only call this after all POIs area added and some content is requested.
      */
     protected function _sortPois() {        
         if ($this->_sorted) return; // POIs are already sorted
         $tmp = array();
-        // cycle throught all POIs, put in array indexed by priority
+        // cycle throught all POIs, put them in array indexed by priority
         foreach ($this->_pois as $poi) {
             $tmp[$poi->getPriority()] = $poi;
         }
@@ -124,9 +136,6 @@ class GSAA_Model_AggregatedPOI
         foreach($tmp as $poi) $this->_pois[] = $poi;
         $this->_sorted = true;
     }
-
-
-    
     
     
 }
