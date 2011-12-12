b75b663f13fc6f4f37c9869581f080052c21dcb5
Ondřej Machulda <ondrej.machulda@gmail.com>
2011-12-13 00:55:49 +0100

    POI model tests

--- tests/application/controllers/UserControllerTest.php
+++ tests/application/controllers/UserControllerTest.php
@@ -7,6 +7,7 @@ class UserControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
     {
         $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
         parent::setUp();
+
     }
 
     public function testIndexAction()
@@ -25,7 +26,7 @@ class UserControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
             'div#view-content p',
             'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
             );
-         * 
+         *
          */
     }
 
---
+++ tests/application/models/PoiModelTest.php
@@ -0,0 +1,57 @@
+<?php
+class PoiModelTest extends PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $this->bootstrap->bootstrap();
+    }
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testEmptyConstructorFails(){
+        new GSAA_Model_POI('');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testConstructorFailsWithNotExistingType(){
+        new GSAA_Model_POI('foobar');
+        new GSAA_Model_POI('bazbar');
+    }
+
+    public function poiTypesProvider() {
+        $bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $var = $bootstrap->getOption('var');
+        $return = array();
+
+        foreach ($var['services'] as $serviceId => $serviceArray) {
+            $return[] = array($serviceId, $serviceArray['priority']);
+        }
+        return $return;
+    }
+    /**
+     * @dataProvider poiTypesProvider
+     */
+    public function testInitWorks($serviceType, $priority){
+        $poi = new GSAA_Model_POI($serviceType);
+        $this->assertInstanceOf('GSAA_Model_POI', $poi);
+    }
+
+    /**
+     * @dataProvider poiTypesProvider
+     */
+    public function testPriorityEquals($serviceType, $priority){
+        $poi = new GSAA_Model_POI($serviceType);
+        $this->assertEquals($priority, $poi->getPriority());
+    }
+
+}
