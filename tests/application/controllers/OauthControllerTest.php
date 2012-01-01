49310b7df6f05e56adf083fdfff4129fe066052b
Ondřej Machulda <ondrej.machulda@gmail.com>
2012-01-01 22:35:34 +0100

    OAuth controller tests

--- tests/application/controllers/OauthControllerTest.php
+++ tests/application/controllers/OauthControllerTest.php
@@ -7,25 +7,73 @@ class OauthControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
     {
         $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
         parent::setUp();
+
+        // default server variables
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+        $_SERVER['HTTP_HOST'] = 'gsaa.local';
     }
 
-    public function testIndexAction()
+    public function testCallbackActionPrintErrorWithInvalidToken()
     {
-        $params = array('action' => 'index', 'controller' => 'Oauth', 'module' => 'default');
+        $params = array('action' => 'callback', 'controller' => 'oauth', 'module' => 'default',
+                        'service' => 'fq',
+                        'code'   => 'INVALID'
+            );
         $urlParams = $this->urlizeOptions($params);
         $url = $this->url($urlParams);
         $this->dispatch($url);
 
-        /*// assertions
+        print_r($this->getResponse()->outputBody());
         $this->assertModule($urlParams['module']);
         $this->assertController($urlParams['controller']);
         $this->assertAction($urlParams['action']);
-        $this->assertQueryContentContains(
-            'div#view-content p',
-            'View script for controller <b>' . $params['controller'] . '</b> and script/action name <b>' . $params['action'] . '</b>'
+
+        $this->assertQueryCount('#document #content p', 2);
+    }
+
+    public function testIsAuthorizedNotDirectlyAccessible()
+    {
+        $params = array('action' => 'is-authorized', 'controller' => 'oauth', 'module' => 'default',
+                        'service' => 'fq'
             );
-         * 
-         */
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+
+        $this->assertController('error');
+        $this->assertResponseCode(500);
+    }
+    public function testIsAuthorizedReturnsFalse()
+    {
+        $this->request->setHeader('X-Requested-With', 'XMLHttpRequest');
+        $params = array('action' => 'is-authorized', 'controller' => 'oauth', 'module' => 'default',
+                        'service' => 'fq'
+            );
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+
+        $responseBody = $this->getResponse()->getBody();
+        $responseValue = Zend_Json::decode($responseBody);
+        $this->assertTrue(is_array($responseValue));
+        $this->assertEquals($responseValue['status'], false);
+
+    }
+
+    public function testDestroyActionIsDispatchedFalse()
+    {
+        $params = array('action' => 'destroy', 'controller' => 'oauth', 'module' => 'default');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
     }
 
 
